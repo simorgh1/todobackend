718ab93dec829d2bac2e9f7abdd19cc1fcf1e815
Bahram Maravandi <simorgh1@users.noreply.github.com>
2023-06-13 12:42:35 +0200

    Security alert fix
    Upgraded the current django packages

--- src/setup.py
+++ src/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup (
   name                 = "todobackend",
-  version              = "0.1.0",
+  version              = "0.1.1",
   description          = "Todobackend Django REST service",
   packages             = find_packages(),
   include_package_data = True,
   scripts              = ["manage.py"],
-  install_requires     = ["Django>=1.9,<2.0",
-                          "django-cors-headers>=1.1.0",
-                          "djangorestframework>=3.3.1",
+  install_requires     = ["Django>=4.0,<4.2",
+                          "django-cors-headers>=4.0.0",
+                          "djangorestframework>=3.14.0",
                           "MySQL-python>=1.2.5",
                           "uwsgi>=2.0"],
   extras_require       = {
